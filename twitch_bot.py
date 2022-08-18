15a69e36fd5b1a5e0e8a709c30dfa346d4736cb5
Hou Wai Wan (Hugh) <wanhouwai@gmail.com>
2022-08-17 18:04:04 -0700

    twitch bot

---
+++ twitch_bot.py
@@ -0,0 +1,31 @@
+import os
+from twitchio.ext import commands
+
+
+class Bot(commands.Bot):
+
+    def __init__(self):
+        # Initialise our Bot with our access token, prefix and a list of channels to join on boot...
+        # prefix can be a callable, which returns a list of strings or a string...
+        # initial_channels can also be a callable which returns a list of strings...
+        super().__init__(token='oauth:jyau8stg0tsob28l8othpdhtusgxkz', prefix='!', initial_channels=['Waikin_'])
+
+    async def event_ready(self):
+        # Notify us when everything is ready!
+        # We are logged in and ready to chat and use commands...
+        print(f'Logged in as | {self.nick}')
+        print(f'User id is | {self.user_id}')
+
+    @commands.command()
+    async def hello(self, ctx: commands.Context):
+        # Here we have a command hello, we can invoke our command with our prefix and command name
+        # e.g ?hello
+        # We can also give our commands aliases (different names) to invoke with.
+
+        # Send a hello back!
+        # Sending a reply back to the channel is easy... Below is an example.
+        await ctx.send(f'Hello {ctx.author.name}!')
+
+
+bot = Bot()
+bot.run()
