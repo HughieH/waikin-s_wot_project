2088527f3c30e4550f24ad4dbdbd8fcf9b828468
Hou Wai Wan (Hugh) <wanhouwai@gmail.com>
2022-08-23 15:03:29 -0700

    testing postgres connector

--- test.py
+++ test.py
@@ -1,18 +1,38 @@
-from twitchio.ext import routines
-import asyncio
+import psycopg2
+
+def connect():
+    """ Connect to the PostgreSQL database server """
+
+    try: 
+
+        # connect to the PostgreSQL server
+        print('Connecting to the PostgreSQL database...')
+        conn = psycopg2.connect(
+            host="localhost",
+            database="postgres",
+            user = "postgres",
+            password="S768022a")
+		
+        # create a cursor
+        cur = conn.cursor()
+        
+	# execute a statement
+        print('PostgreSQL database version:')
+        cur.execute('SELECT version()')
+
+        # display the PostgreSQL database server version
+        db_version = cur.fetchone()
+        print(db_version)
+       
+	# close the communication with the PostgreSQL
+        cur.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+            print('Database connection closed.')
 
 
-@routines.routine(seconds=5.0, iterations=5)
-async def hello(arg: str):
-    print(f'Hello {arg}!')
-
-loop = asyncio.get_event_loop()
-try:
-    asyncio.ensure_future(hello.start("gamer"))
-    loop.run_forever()
-except KeyboardInterrupt:
-    pass
-finally:
-    print("Closing Loop")
-    loop.close()
-
+if __name__ == '__main__':
+    connect()
